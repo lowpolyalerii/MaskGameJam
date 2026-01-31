ae0f9d0a221bf56926986ddc02569b7c4e3040c3
AESA <145212470+lowpolyalerii@users.noreply.github.com>
2026-01-31 19:38:06 +0000

    Fixed Borders

---
+++ Assets/EnemyFollow.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.linearVelocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+}
